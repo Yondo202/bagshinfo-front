048f1c6d4f1bb7cbc9b7b34fe6b084eb33e536a0
Yondo202 <yondooo61@gmail.com>
2022-01-09 15:21:37 +0800

    dd

---
+++ miscs/theme.js
@@ -0,0 +1,44 @@
+export const topWidth = 'width:1400px; @media (max-width:1400px){ width:100%; }'
+
+export const fontSize = '14px';
+export const fontSize2 = '16px';
+
+export const fontFamily = `raleway`;
+export const fontFamily2 = `merriweather`;
+
+// export const mainColor = "#334195";
+export const mainColor = '#ffcb00';
+export const bodyColor = '#fffefa';
+export const mainColor2 = "#4d4d4d";
+
+export const fontSizeSmSm='12px';
+export const fontSizeSm='13px';
+export const fontSizeMd='14px';
+export const fontSizeBig='15px';
+
+export const headerHeight = "154px";
+export const headerHeightMobile = "52px";
+
+
+export const  textColor= '#000f23'; // arai tod ( title, link  ) -  'undsen text'
+export const  textColor2='#001022'; // ( button )
+export const  textColor3='#495460'; // some opocity ( icon, description )
+export const  textColor4='#767676'; // opacity ++ ( desable text, )
+
+export const fontSizeExtra = '1.5rem';
+export const fontSizeExtra2 = '1.7rem';
+
+export const MainMarginBottom = 110
+
+export const MainBorderStyle = ` content:'';
+position:absolute;
+top:0.6rem;
+left:0.6rem;
+bottom:0.6rem;
+right:0.6rem;
+border: 0.19rem solid #ffcb00;
+z-index: 3;`
+
+export const weight = "font-family: raleway-medium;"
+export const weight2 = "font-family: raleway-semibold;"
+export const weight3 = "font-family: raleway-bold;"
